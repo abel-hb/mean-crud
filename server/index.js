5b4f8d2b9c7361981b2a57693b8a5dad13215708
abel-hb <abelherrero.17@campuscamara.es>
2020-01-22 18:24:04 +0100

    [ADD CORS TO SERVER]

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const { mongoose } = require('./database');
+const cors = require('cors');
 
 //Settings  Configuraciones basica del server
 app.set('port',process.env.PORT || 3000);//Esto es para coger los puertos por defecto
@@ -13,7 +14,7 @@ app.set('port',process.env.PORT || 3000);//Esto es para coger los puertos por de
 //Middlewares Trnajar con la imformacion del servidor
 app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(cors({origin: 'http://localhost:4200'}));
 
 //Routes
 app.use('/api/teachers', require('./routers/teachers.routers'));

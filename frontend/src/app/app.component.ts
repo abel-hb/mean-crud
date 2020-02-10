6e8f226a71028d479a355a82cb07d6b49db25d7c
abel-hb <abelherrero.17@campuscamara.es>
2020-02-10 19:47:42 +0100

    [COMMIT] UPDATE CODE APPCOMPONENT

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { UserService } from './services/users.service';
 import { User } from './models/user';
 import { NgForm } from '@angular/forms';
 import { error } from 'util';
+import { Router } from '@angular/router';
 
 declare var M: any;
 
@@ -19,7 +20,7 @@ export class AppComponent {
   public identity = null;
   public token = null;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService,private router: Router) {
     this.user = new User('', '', '', '', '', 'ROLE_USER', '');
     this.user_register = new User('', '', '', '', '', 'ROLE_USER', '');
   }
@@ -86,6 +87,7 @@ export class AppComponent {
     localStorage.clear();
     this.identity = null;
     this.token = null;
+    this.router.navigate(['/'])
     M.toast({ html: 'Logout successfully, thanks for you visit' });
   }
 
@@ -109,7 +111,7 @@ export class AppComponent {
         this.user_register = new User('', '', '', '', '', 'ROLE_USER', '');
        }
      },
-     error => {}
+     error => {M.toast({ html: 'Not Register' });}
      
      )
   }
